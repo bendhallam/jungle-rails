c64e42761965cc964ed8ff0bc8cfdd6495b2cac7
Benjamin <bendhallam@gmail.com>
2024-11-24 16:19:51 -0700

    added counts to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    # Fetch the counts of products and categories
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
